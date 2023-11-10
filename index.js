cddc1ce58f0f1d2f2e4d0698d94f159b28ff916a
Mostafa Kheyrollahi <mostafa.kheyrollahi@infrafibre-networks.de>
2023-11-10 10:27:22 +0100

    without php

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path');
-const router = express.Router();
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// const path = require('path');
+// const router = express.Router();
 
 
-app.use(express.static('public'))
-app.use(express.static('node_modules'))
-app.use(express.static('img'))
+// app.use(express.static('public'))
+// app.use(express.static('node_modules'))
+// app.use(express.static('img'))
 
-app.get('/users',callName);
-
-
-function callName( req, res ) {
-var spawn = require('child_process').spawn;
-//let command = spawn("php", [ "./index.php",req.query.db, req.query.shape, req.query.crm]);
-let command = spawn("php", [ path.join(__dirname + '/index.php'),req.query.db, req.query.shape, req.query.crm]);
-  command.stdout.pipe(res)
-  command.on("close", function (data) {
-    console.log("done writing");
-  });
-};
-
-
-
-
-
-router.get('/', function(req, res) { 
-	res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.use('/', router); 
-app.listen(process.env.port || 3000);
-console.log('Running at Port 3000');
-
-//------------------------------------------------------------------------------
-// const express = require('express');
-// var app = express()
-  // , server = require('https').createserver(app)
-  // , io = io.listen(server);
-  
 // app.get('/users',callName);
 
 
@@ -49,58 +18,10 @@ console.log('Running at Port 3000');
   // command.stdout.pipe(res)
   // command.on("close", function (data) {
     // console.log("done writing");
-  // });
-// };
+   // });
+ // };
 
 
-// app.use(express.static('public'))
-// app.use(express.static('node_modules'))
-// app.use(express.static('img'))
-
-
-// app.get('/', function(req, res) {
-  // res.sendfile('./index.html');
-// });
-// server.listen(80);
-
-//------------------------------------------------------------------------------
-
-// const express = require('express')
-// const app = express()
-// const port = 3000
-// const path = require('path');
-// const router = express.Router();
-// var pg = require('pg');
-
-// var connectionString = "postgres://BBVGISAdmin:BBVQGIS01@sgeoportal.bbv-deutschland.de:63303/BBVCRMMirror";
-// var pgClient = new pg.Client(connectionString);
-// pgClient.connect();
-// //var query = pgClient.query("SELECT *, ST_AsGeoJSON(ST_Transform(geometry, 4326)) As geojson FROM crm_nok_gee_mirror");
-
-// app.get('/users',callName);
-
-// function callName(req, res) {
-	// var gebiet = req.query.level;
-	// qu = pgClient.query('SELECT *, ST_AsGeoJSON(ST_Transform(geometry, 4326)) As geojson FROM crm_'+gebiet+'_gee_mirror limi 5');
-	// res.send({
-    // 'geo': nok
-  // });
-// };
-
-
-// // function callName( req, res ) {
-// // var spawn = require('child_process').spawn;
-// // let command = spawn("php", [ "./index.php",req.query.db, req.query.shape, req.query.crm]);
-  // // command.stdout.pipe(res)
-  // // command.on("close", function (data) {
-    // // console.log("done writing");
-  // // });
-// // };
-
-
-// app.use(express.static('public'))
-// app.use(express.static('node_modules'))
-// app.use(express.static('img'))
 
 
 // router.get('/', function(req, res) { 
@@ -110,3 +31,79 @@ console.log('Running at Port 3000');
 // app.use('/', router); 
 // app.listen(process.env.port || 3000);
 // console.log('Running at Port 3000');
+
+//------------------------------------------------------------------------------
+//Chatgpt
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const { Pool } = require('pg');
+const pgp = require('pg-promise')();
+const fs = require('fs');
+
+
+
+const app = express();
+const port = 3000; // Set the port you want to use
+
+app.use(express.static('public'))
+app.use(express.static('node_modules'))
+app.use(express.static('img'))
+
+
+
+app.get('/get-layer/:database/:shape/:table', (req, res) => {
+	const database = req.params.database;
+	const shape = req.params.shape;
+	const table = req.params.table;
+	var geomshape = '';
+	if (shape == 'point') {
+        geomshape = 'geometry'
+	} else {
+		geomshape = 'geom'
+	}
+	
+	var connectionConfig = {
+		user: 'BBVGISAdmin',
+		database: database,
+		password: 'BBVQGIS01',
+		host: 'geoportal.bbv-deutschland.de', // Change this to your database server host
+		port: 63303,      // Change this to your database server port
+	};
+	var db = pgp(connectionConfig);
+	
+  db.any('SELECT *, ST_AsGeoJSON(ST_Transform('+geomshape+', 4326)) as geometry FROM '+table+' ')
+    .then((data) => {
+      const features = data.map((row) => ({
+        type: 'Feature',
+        geometry: JSON.parse(row.geometry),
+        properties: {
+			 ...row
+        },
+      }));
+	  
+	  const geojsonData = {
+        type: 'FeatureCollection',
+        features,
+      };
+		
+    const geojsonString = JSON.stringify(geojsonData);
+		res.send(geojsonString);
+     });
+});
+//----------------------
+
+router.get('/', function(req, res) { 
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use('/', router); 
+
+// app.listen(port, () => {
+  // console.log(`Server is listening on port ${port}`);
+// });
+
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
+
+
