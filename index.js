abefe1d68cda2425e7bf834586b39dbc4ab01207
Mostafa Kheyrollahi <mostafa.kheyrollahi@infrafibre-networks.de>
2024-02-01 15:27:42 +0100

    1.2.2024

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const express = require('express');
 const router = express.Router();
@@ -9,6 +8,10 @@ const app = express();
 const port = 3000; // Set the port you want to use
 const axios = require('axios');
 const convert = require('xml-js');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const nodemailer = require('nodemailer');
 //----------------------------------------------------------------------------------------
 
 app.use(express.static('styles'))
@@ -17,6 +20,197 @@ app.use(express.static('img'))
 app.use(express.static('projects'))
 app.use(express.static('libraries'))
 //--------------------------------------------------------------------------------------------
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(session({
+	secret: '*****',
+	resave: false,
+	saveUninitialized: true,
+}));
+
+const pool = new Pool({
+	user: '***********',
+	host: '***************',
+	database: '******************',
+	password: '*******************',
+	port: 63303,
+});
+
+const authenticate = (req, res, next) => {
+	if (req.session && req.session.user) {
+    // User is authenticated, proceed to the next middleware or route
+		return next();
+	} else {
+    // Redirect to the login page if the user is not authenticated
+		req.session.requestedUrl = req.originalUrl;
+		res.redirect('/signin');
+	}
+};
+
+app.get('/home', authenticate,(req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/signin', (req, res) => {
+	if (req.session.user) {
+		return res.redirect('/home');
+	}else{
+		res.sendFile(__dirname + '/signin.html');
+	}
+});
+
+
+
+app.get('/signup', (req, res) => {
+	if (req.session.user) {
+		return res.redirect('/home');
+	}else{
+		res.sendFile(__dirname + '/signup.html');
+	}
+});
+
+const transporter = nodemailer.createTransport({
+	host: 'smtp.office365.com',
+	port: 587,
+	secure: false,
+	auth: {
+		user: 'mostafa.kheyrollahi@infrafibre-networks.de', // Your Outlook email address
+		pass: '*********************', // Your Outlook email password
+	},
+});
+
+const verificationCodes = {};
+let signupUserInfo = [];
+app.post('/signup', async (req, res) => {
+	const { name, username, password } = req.body;
+	
+
+	if (!name || !username || !password) {
+		return res.json({ status: 'error', error: 'Name, username and password are required.' })
+	}
+	
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+
+	if (!password || typeof password !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (password.length < 6) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be at least 6 characters'
+		})
+	}
+	
+	try {
+		const userExists = await pool.query('SELECT * FROM users WHERE username_email = $1', [username]);
+
+		if (userExists.rows.length > 0) {
+			return res.json({ status: 'error', error: 'Username already exists. Please choose another one.' })
+		}
+		
+		const verificationCode = Math.floor(100000 + Math.random() * 900000);
+		
+		 req.session.registrationInfo = {
+			username: req.body.username,
+			password: req.body.password,
+			verificationCode,
+		};
+		
+		const mailOptions = {
+			from: 'mostafa.kheyrollahi@infrafibre-networks.de',
+			to: username,
+			subject: 'Verification Code for Signup',
+			text: `Your verification code is: ${verificationCode}`,
+		};
+
+		await transporter.sendMail(mailOptions);
+		verificationCodes[username] = verificationCode;
+		signupUserInfo = [ name, username, password ];
+		res.redirect('/verify');
+
+	} catch (error) {
+		console.error('Error during signup:', error);
+		return res.json({ status: 'error', error: 'Internal Server Error' })
+	}
+});
+
+app.get('/verify', (req, res) => {
+	if (req.session.user) {
+		return res.redirect('/home');
+	}else if (!req.session.registrationInfo) {
+		res.redirect('/signup');
+	}else{
+		res.sendFile(__dirname + '/verify.html');
+	}
+});
+
+app.post('/verify', async(req, res) => {
+	const { verificationCode } = req.body;
+
+	// Retrieve the stored verification code
+	const storedVerificationCode = verificationCodes[signupUserInfo[1]];
+
+	if (!storedVerificationCode || verificationCode !== storedVerificationCode) {
+		return res.json({ status: 'error', error: 'Invalid verification code.' });
+	}
+
+	// Verification successful, insert the user into the database
+	await pool.query('INSERT INTO users (personal_name, username_email, password, usertype) VALUES ($1, $2, $3, $4)', [signupUserInfo[0], signupUserInfo[1], signupUserInfo[2], 'User']);
+
+	// Clear the stored verification code
+	delete verificationCodes[signupUserInfo[1]];
+	signupUserInfo.length = 0;
+	delete req.session.registrationInfo;
+	// Redirect to a success page or login page
+	res.redirect('/signin');
+});
+
+
+app.post('/signin', async (req, res) => {
+	const { username, password } = req.body;
+
+	if (!username || !password) {
+		return res.json({ status: 'error', error: 'Username and password are required.' });
+	}
+
+	try {
+		const result = await pool.query('SELECT * FROM users WHERE username_email = $1 AND password = $2', [
+			username,
+			password,
+		]);
+
+		if (result.rows.length === 0) {
+			//return res.status(401).send('Invalid username or password.');
+			return res.json({ status: 'error', error: 'Invalid username or password.'});
+		}
+
+		// Store user information in the session
+		req.session.user = { username: result.rows[0].username};
+
+		const requestedUrl = req.session.requestedUrl;
+		delete req.session.requestedUrl; // Clear the stored URL
+
+		if (requestedUrl && (requestedUrl == req.session.requestedUrl || '/:subfolder/:qgisProject')) {
+			res.json({ status: 'ok', redirectUrl: requestedUrl });
+		} else {
+			res.json({ status: 'ok', redirectUrl: '/home' });
+		}
+	} catch (error) {
+		console.error('Error during signin:', error);
+		return res.json({ status: 'error', error: 'Internal Server Error'});
+	}
+});
+
+
+
+
+
+//---------------------------------------------------------------------------------------------
 
 // Put all of the project names into a dict
 
@@ -83,12 +277,13 @@ const {projectsDict, projectsImg} = getQGISProjectsDict(directoryPath);
 app.get('/projectsDict', (req, res) => {
 	res.json(projectsDict);
 });
+
 app.get('/projectsImg', (req, res) => {
 	res.json(projectsImg);
 });
 
 var absololute_dir = {}
-absololute_dir['dir'] = directoryPath
+absololute_dir['dir'] = directoryPath;
 app.get('/directoryPath', (req, res) => {
 	res.json(absololute_dir);
 });
@@ -130,8 +325,8 @@ app.get('/:subfolder/:qgisProject/getQgsConf', (req, res) => {
 });
 
 //--------------------------------------------------------------------------------
-//var domain = "http://localhost:9000"
-var domain = "https://bbvnewgeoportal.onrender.com";
+var domain = "http://localhost:9000"
+//var domain = "https://bbvnewgeoportal.onrender.com";
 
 app.get('/:subfolder/:qgisProject/getWFS/:layername', async (req, res) => {
 	const mainProject = req.params.subfolder;
@@ -156,17 +351,35 @@ app.get('/:subfolder/:qgisProject/getWFS/:layername', async (req, res) => {
  
 //--------------------------------------------------------------------------------
 
-router.get('/:subfolder/:qgisProject', function(req, res) {
+router.get('/:subfolder/:qgisProject', authenticate,(req, res) =>{
 	const mainProject = req.params.subfolder;
 	const QGISProject = req.params.qgisProject;
 	const qgsFilePath = __dirname+'/projects/'+mainProject+'/'+QGISProject+'.qgs';
 	const qgsConfPath = __dirname+'/projects/'+mainProject+'/'+QGISProject+'.qgs.cfg';
+
 	if ((fs.existsSync(qgsFilePath)) && (fs.existsSync(qgsConfPath))) {
 		res.sendFile(path.join(__dirname + '/index.html'));
 	}else{
-		res.status(404).send('The QGIS project or its config file are not found');
+		//res.status(404).send('The QGIS project or its config file are not found');
+		return res.json({ status: 'error', error: 'The QGIS project or its config file are not found'});
 	}
 });
+//------------------------------------------------------------------------------------
+
+app.get('/signout', (req, res) => {
+    // Destroy the user session
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Error destroying session:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        // Redirect to the sign-in page after successful sign-out
+        res.redirect('/signin');
+    });
+});
+
+//-------------------------------------------------------------------------------------
+
 
 router.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/wmtstest.html'));
